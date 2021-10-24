ea9561884604d46bc9b22b5a8771e0ed7ad8ac88
Felipe Ribeiro <feliper.dev@gmail.com>
2021-10-23 23:54:39 -0300

    Preparing enviroment to start the changes at `AboutApp` page

--- lib/modules/info/presenter/about_app.page.dart
+++ lib/modules/info/presenter/about_app.page.dart
@@ -1,8 +1,31 @@
+import 'package:anime_work_time_management/core/app_settings.dart';
+import 'package:anime_work_time_management/modules/info/controllers/info_controller.dart';
+import 'package:anime_work_time_management/shared/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class AboutAppPage extends StatelessWidget {
+  final settings = Modular.get<SettingsController>();
+  final controller = Modular.get<InfoController>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    bool? mode = settings.switchMode!;
+    Color? bgColor = (mode ? Colors.white : AppColors.mainTimerColorDark);
+    Color? mainColor = (mode ? AppColors.mainTimerColorLight : Colors.white);
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: bgColor,
+        iconTheme: IconThemeData(
+          color: mainColor,
+        ),
+        centerTitle: false,
+        title: Text(
+          'About App',
+          style: TextStyle(color: mainColor),
+        ),
+      ),
+    );
   }
 }
